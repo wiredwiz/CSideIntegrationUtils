992aa2c68f52b49cbdf7425680a5f61138f1773f
Thad Ryker <thad@edgerunner.org>
2013-04-03 23:35:23 -0400

    Added more documentation to ClientLink

--- ClientLink.cs
+++ ClientLink.cs
@@ -1,8 +1,24 @@
-﻿using System;
+﻿//
+// Copyright 2010 Thaddeus L Ryker
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Dynamics Nav is a registered trademark of the Microsoft Corporation
+//
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Runtime.InteropServices;
 
 namespace Org.Edgerunner.Dynamics.Nav.CSide
 {
@@ -349,6 +365,7 @@ namespace Org.Edgerunner.Dynamics.Nav.CSide
          return true;
       }
 
+      /// <summary>Represents the different network connection types used for connecting to Microsoft Dynamics Nav.</summary>
       public enum ConnectionNetType
       {
          @default,
@@ -359,6 +376,7 @@ namespace Org.Edgerunner.Dynamics.Nav.CSide
          sockets
       }
 
+      /// <summary>Represents the two Microsoft Dynamics Nav server types used for client connections.</summary>
       public enum ConnectionServerType
       {
          MSSQL,
