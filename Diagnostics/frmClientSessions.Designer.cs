de17c3680659c64eacaf82e2e9c41bd8ba77c655
Thaddeus Ryker <thaddeus.ryker@dansko.com>
2013-10-11 15:57:00 -0400

    Started Adding diagnostic tools to the library for feature testing as well as for demonstration.

---
+++ Diagnostics/frmClientSessions.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CSide_Library_Diagnostics_Tool
+{
+   partial class frmClientSessions
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.components = new System.ComponentModel.Container();
+         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmClientSessions));
+         this.timer1 = new System.Windows.Forms.Timer(this.components);
+         this.lstClients = new System.Windows.Forms.ListView();
+         this.colServer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+         this.colDatabase = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+         this.colCompany = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+         this.btnOpenDiagnostic = new System.Windows.Forms.Button();
+         this.colType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+         this.SuspendLayout();
+         // 
+         // timer1
+         // 
+         this.timer1.Interval = 3000;
+         this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+         // 
+         // lstClients
+         // 
+         this.lstClients.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colType,
+            this.colServer,
+            this.colDatabase,
+            this.colCompany});
+         this.lstClients.FullRowSelect = true;
+         this.lstClients.Location = new System.Drawing.Point(12, 35);
+         this.lstClients.Name = "lstClients";
+         this.lstClients.Size = new System.Drawing.Size(514, 222);
+         this.lstClients.TabIndex = 2;
+         this.lstClients.UseCompatibleStateImageBehavior = false;
+         this.lstClients.View = System.Windows.Forms.View.Details;
+         this.lstClients.DoubleClick += new System.EventHandler(this.lstClients_DoubleClick);
+         // 
+         // colServer
+         // 
+         this.colServer.Text = "Server";
+         this.colServer.Width = 100;
+         // 
+         // colDatabase
+         // 
+         this.colDatabase.Text = "Database";
+         this.colDatabase.Width = 150;
+         // 
+         // colCompany
+         // 
+         this.colCompany.Text = "Company";
+         this.colCompany.Width = 200;
+         // 
+         // btnOpenDiagnostic
+         // 
+         this.btnOpenDiagnostic.Location = new System.Drawing.Point(12, 263);
+         this.btnOpenDiagnostic.Name = "btnOpenDiagnostic";
+         this.btnOpenDiagnostic.Size = new System.Drawing.Size(104, 40);
+         this.btnOpenDiagnostic.TabIndex = 3;
+         this.btnOpenDiagnostic.Text = "Open Diagnostic";
+         this.btnOpenDiagnostic.UseVisualStyleBackColor = true;
+         this.btnOpenDiagnostic.Click += new System.EventHandler(this.btnOpenDiagnostic_Click);
+         // 
+         // colType
+         // 
+         this.colType.Text = "Type";
+         // 
+         // frmClientSessions
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(537, 315);
+         this.Controls.Add(this.btnOpenDiagnostic);
+         this.Controls.Add(this.lstClients);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+         this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+         this.Name = "frmClientSessions";
+         this.Text = "C/Side Clients";
+         this.Load += new System.EventHandler(this.frmClientSessions_Load);
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Timer timer1;
+      private System.Windows.Forms.ListView lstClients;
+      private System.Windows.Forms.ColumnHeader colServer;
+      private System.Windows.Forms.ColumnHeader colDatabase;
+      private System.Windows.Forms.ColumnHeader colCompany;
+      private System.Windows.Forms.Button btnOpenDiagnostic;
+      private System.Windows.Forms.ColumnHeader colType;
+   }
+}
+
---
+++ Diagnostics/frmDiagnostic.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSide_Library_Diagnostics_Tool
+{
+   public partial class frmDiagnostic : Form
+   {
+      Org.Edgerunner.Dynamics.Nav.CSide.Client _Client;
+
+      public frmDiagnostic()
+      {
+         InitializeComponent();
+      }
+
+      public void SetClient(Org.Edgerunner.Dynamics.Nav.CSide.Client client)
+      {
+         _Client = client;
+         Text = string.Format("Diagnostic - {0}", _Client.Company);
+         UpdateClientData();
+         _Client.Activated += new EventHandler<Org.Edgerunner.Dynamics.Nav.CSide.CSideEventArgs>(_Client_Activated);
+         _Client.Deactivated += new EventHandler<Org.Edgerunner.Dynamics.Nav.CSide.CSideEventArgs>(_Client_Deactivated);
+         _Client.CompanyChanged += new EventHandler<Org.Edgerunner.Dynamics.Nav.CSide.CSideEventArgs>(_Client_CompanyChanged);
+         _Client.DatabaseChanged += new EventHandler<Org.Edgerunner.Dynamics.Nav.CSide.CSideEventArgs>(_Client_DatabaseChanged);
+         _Client.ServerChanged += new EventHandler<Org.Edgerunner.Dynamics.Nav.CSide.CSideEventArgs>(_Client_ServerChanged);
+         _Client.FormOpened += new EventHandler<Org.Edgerunner.Dynamics.Nav.CSide.CSideEventArgs>(_Client_FormOpened);
+      }
+
+      void _Client_FormOpened(object sender, Org.Edgerunner.Dynamics.Nav.CSide.CSideEventArgs e)
+      {
+         LogData(string.Format("Form {0} opened", e.Form));
+      }
+
+      void _Client_Deactivated(object sender, Org.Edgerunner.Dynamics.Nav.CSide.CSideEventArgs e)
+      {
+         LogData("Client window deactivated");
+      }
+
+      void _Client_ServerChanged(object sender, Org.Edgerunner.Dynamics.Nav.CSide.CSideEventArgs e)
+      {
+         LogData(string.Format("Client server changed from {0} to {1}", e.PreviousServer, e.CurrentServer));
+      }
+
+      void _Client_DatabaseChanged(object sender, Org.Edgerunner.Dynamics.Nav.CSide.CSideEventArgs e)
+      {
+         LogData(string.Format("Client database changed from {0} to {1}", e.PreviousDatabase, e.CurrentDatabase));
+      }
+
+      void _Client_CompanyChanged(object sender, Org.Edgerunner.Dynamics.Nav.CSide.CSideEventArgs e)
+      {
+         LogData(string.Format("Client company changed from {0} to {1}", e.PreviousCompany, e.CurrentCompany));
+      }
+
+      void _Client_Activated(object sender, Org.Edgerunner.Dynamics.Nav.CSide.CSideEventArgs e)
+      {
+         LogData("Client window activated");
+      }
+
+      private void LogData(string logText)
+      {
+         if (lstLog.Items.Count > 200)
+            lstLog.Items.RemoveAt(0);
+         lstLog.Items.Add(string.Format("[{0}] {1}", DateTime.Now.ToShortTimeString(), logText));
+      }
+
+      private void UpdateClientData()
+      {
+         txtServerType.Text = _Client.ServerType.ToString();
+         txtServerName.Text = _Client.Server;
+         txtDatabaseName.Text = _Client.Database;
+         txtCompanyName.Text = _Client.Company;
+         txtCSideVersion.Text = _Client.CSideVersion;
+         txtAppVersion.Text = _Client.ApplicationVersion;
+      }
+
+      private void btnRefresh_Click(object sender, EventArgs e)
+      {
+         UpdateClientData();
+      }
+   }
+}
