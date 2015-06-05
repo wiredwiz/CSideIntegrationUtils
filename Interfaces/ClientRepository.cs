b4b149a176b0547f65393a7fe50160f9dd161ba7
Thaddeus Ryker <Thaddeus.Ryker@dansko.com>
2015-06-05 15:14:16 -0400

    Added ClientRepository and began the programming of it.

---
+++ Interfaces/ClientRepository.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace Org.Edgerunner.Dynamics.Nav.CSide.Interfaces
+{
+   public class ClientRepository
+   {
+      private Dictionary<object, Client> _RunningClients;
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="ClientRepository"/> class.
+      /// </summary>
+      public ClientRepository()
+      {
+         _RunningClients = new Dictionary<object, Client>();
+         var clients = GetActiveClientList();
+         
+      }
+
+      [DllImport("user32.dll", SetLastError = true)]
+      private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+
+      /// <summary>
+      /// Returns a pointer to the IRunningObjectTable interface on the local running object table (ROT).
+      /// </summary>
+      /// <param name="reserved">This parameter is reserved and must be 0.</param>
+      /// <param name="prot">The address of an IRunningObjectTable* pointer variable that receives the interface pointer to the local ROT.
+      /// When the function is successful, the caller is responsible for calling Release on the interface pointer. If an error occurs, *pprot is undefined.</param>
+      [DllImport("ole32.dll")]
+      private static extern void GetRunningObjectTable(int reserved, out IRunningObjectTable prot);
+
+      /// <summary>
+      /// Returns a pointer to an implementation of IBindCtx (a bind context object).
+      /// This object stores information about a particular moniker-binding operation.
+      /// </summary>
+      /// <param name="reserved">This parameter is reserved and must be 0.</param>
+      /// <param name="ppbc">Address of an IBindCtx* pointer variable that receives
+      /// the interface pointer to the new bind context object. When the function is
+      /// successful, the caller is responsible for calling Release on the bind context.
+      /// A NULL value for the bind context indicates that an error occurred.</param>
+      /// <returns>This function can return the standard return values E_OUTOFMEMORY and S_OK.</returns>
+      [DllImport("ole32.dll")]
+      private static extern void CreateBindCtx(int reserved, out IBindCtx ppbc);
+
+      /// <summary>
+      /// Gets the active client list.
+      /// </summary>
+      /// <returns>A list of designer objects corresponding to running client instances</returns>
+      /// <remarks>If there are multiple instances with the same database and company, only the first is exposed</remarks>
+      internal static List<object> GetActiveClientList()
+      {
+         List<object> clientList = new List<object>();
+
+         IntPtr numFetched = IntPtr.Zero;
+         IRunningObjectTable runningObjectTable = null;
+         IEnumMoniker monikerEnumerator = null;
+         IBindCtx ctx = null;
+         IMoniker[] monikers = new IMoniker[1];
+
+         try
+         {
+            GetRunningObjectTable(0, out runningObjectTable);
+            runningObjectTable.EnumRunning(out monikerEnumerator);
+            monikerEnumerator.Reset();
+            int clientNo = 0;
+            while (monikerEnumerator.Next(1, monikers, numFetched) == 0)
+            {
+               CreateBindCtx(0, out ctx);
+
+               string runningObjectName;
+               monikers[0].GetDisplayName(ctx, null, out runningObjectName);
+
+               object runningObjectVal;
+               runningObjectTable.GetObject(monikers[0], out runningObjectVal);
+
+               if ((runningObjectName.IndexOf("!C/SIDE!navision://client/run?") != -1) &&
+                   !clientList.Contains(runningObjectVal))
+               {
+                  clientNo += 1;
+                  clientList.Add(runningObjectVal);
+               }
+            }
+         }
+         finally
+         {
+            // Free resources
+            if (runningObjectTable != null)
+               Marshal.ReleaseComObject(runningObjectTable);
+            if (monikerEnumerator != null)
+               Marshal.ReleaseComObject(monikerEnumerator);
+            if (ctx != null)
+               Marshal.ReleaseComObject(ctx);
+         }
+
+         return clientList;
+
+      }
+   }
+}
